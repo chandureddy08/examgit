30124e8a22c5b346db8e562d835dbbb450966770
chandureddy08 <abc@example.com>
2024-04-17 13:52:03 -0400

    this is py file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 this is first line.
 this is second line by git edit
 this is third line
-####git remote set-url origin https://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@github.com/chandureddy08/examgit.git
+this is fourth line
