2f1e1da10a3038795eb0990e82d1a75095210c4d
chandureddy08 <chandrareddyerigela@example.com>
2024-04-17 16:31:07 -0400

    variables script

--- 3variable.sh
+++ 3variable.sh
@@ -5,5 +5,5 @@ read USERNAME   #user name is variable
 echo "Please enter your Password::"
 read PASSWORD
 
-echo "Username is: $USERNAME, Password is $Password"
+echo "Username is: $USERNAME, Password is $PASSWORD"
 
