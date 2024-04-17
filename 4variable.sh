bd27a1eb8046b1292abe36d24835ada770d78d3c
chandureddy08 <chandrareddyerigela@example.com>
2024-04-17 17:36:51 -0400

    array

---
+++ 4variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read -s USERNAME   #user name is variable
+
+echo "Please enter your Password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is $PASSWORD"
---
+++ array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("ant" "bat" "cat")
+# size of array is 3
+# list always starts with 0
+# index are 0,1 and 2
+
+echo "First movie is: ${MOVIE[0]}"
