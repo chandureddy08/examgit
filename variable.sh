333b289dba30ed852b577cf621ac4f72d55f0fe7
chandureddy08 <chandrareddyerigela@example.com>
2024-04-17 15:58:46 -0400

    variables script

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=chandu
+PERSON2=john
+
+echo "$PERSON1: Hello $PERSON2 How are you"
+echo "$PERSON2: Hi $PERSON1 I'm fine. How are you?"
+echo "$PERSON1: I'am fine too. How's your work?"
+echo "$PERSON2: Not bad dear."
