9239da2bd5199723a2538d7893a02d03a55de756
chandureddy08 <chandrareddyerigela@example.com>
2024-04-18 16:16:46 -0400

    shell practice

--- 12-colors.sh
+++ 12-colors.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-echo -e "$Y Script started executing at: $TIMESTAMP $N"
+echo -e "${Y} Script started executing at: $TIMESTAMP ${N}"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
